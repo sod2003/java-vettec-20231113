3a3f0094f9159fd2fcc0c900792388d51ca339de
sod2003 <sod2003@gmail.com>
2023-11-17 13:41:24 -0500

    Adding my MakeCow program from Day 1

---
+++ MakeCow.java
@@ -0,0 +1,14 @@
+package com.skillstorm.training;
+
+public class MakeCow {
+
+	public static void main(String[] args) {
+		System.out.println("\\|/          (__)    \n"
+				+ "     `\\------(oo)\n"
+				+ "       ||    (__)\n"
+				+ "       ||w--||     \\|/\n"
+				+ "   \\|/");
+
+	}
+
+}
