f561212449e0dd19830f29b0d2f67f78b955a5f6
sod2003 <sod2003@gmail.com>
2024-01-02 16:31:39 -0500

    Added peek, isEmpty and toString methods to MyStack.

--- Week07/Stack/MyStack.java
+++ Week07/Stack/MyStack.java
@@ -29,15 +29,43 @@ public class MyStack<T> {
         return data;
     }
 
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return top.getData();
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
     public int getSize() {
         return size;
+    }
+
+    @Override
+    public String toString() {
+        String str = "[";
+        Node<T> currNode = top;
+        while (currNode != null) {
+            str += currNode.getData();
+            if (currNode.getNext() != null) {
+                str += ", ";
+            }
+            currNode = currNode.getNext();
+        }
+        return str += "]";
     }
     
     public static void main(String[] args) {
         MyStack<Integer> ms = new MyStack<Integer>();
-        System.out.println("Stack has size " + ms.getSize());
+        System.out.println("Stack has size " + ms.getSize() + ".\n" + ms);
         ms.push(1);
-        System.out.println("Stack has size " + ms.getSize());
-        System.out.println("Stack's top data is " + ms.pop());
+        System.out.println("Stack has size " + ms.getSize() + ".\n" + ms);
+        System.out.println("Stack's top data is " + ms.peek());
     }
 }
