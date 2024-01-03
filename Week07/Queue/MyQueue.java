38864166ffdbb268ff74913ce6cb99895ea0c0e7
sod2003 <sod2003@gmail.com>
2024-01-03 11:37:47 -0500

    Adding Queue subfolder to Week07 folder. Includes MyQueue and Node implementations.

---
+++ Week07/Queue/MyQueue.java
@@ -0,0 +1,62 @@
+package Week07.Queue;
+
+public class MyQueue<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public MyQueue() {}
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value);
+        add(node);
+    }
+
+    public void add(Node<T> value) {
+        if (head == null) {
+            head = tail = value;
+        } else {
+            tail.setNext(value);
+            value.setPrev(tail);
+            tail = value;
+        }
+    }
+
+    public T peek() {
+        if (head == null) {
+            throw new IllegalArgumentException("There's nothing in the Queue!"); 
+        }
+        return head.getValue();
+    }
+
+    public T poll() {
+        if (head == null) {
+            throw new IllegalArgumentException("There's nothing in the Queue!"); 
+        }
+        Node<T> removedNode = head;
+        head = head.getNext();
+        return removedNode.getValue();
+    }
+
+    @Override
+    public String toString() {
+        String str = "[";
+        Node<T> currNode = head;
+        while (currNode != null) {
+            str += currNode.getValue();
+            if (currNode.hasNext()) {
+                str += ", ";
+            }
+            currNode = currNode.getNext();
+        }
+        str += "]";
+        return str;
+    }
+
+    public static void main(String[] args) {
+        MyQueue<String> strings = new MyQueue<>();
+        strings.add("Me");
+        strings.add("Myself");
+        strings.add("and I");
+        System.out.println("My strings\n" + strings);
+    }
+}
