ea71b4625050ea70361d94541e6e40b6d5aa4bd5
sod2003 <sod2003@gmail.com>
2024-01-09 11:14:25 -0500

    Created HashMaps subfolder and HashMapPractice file.

---
+++ Week08/HashMaps/HashMapPractice.java
@@ -0,0 +1,12 @@
+package Week08.HashMaps;
+import java.util.HashMap;
+
+public class HashMapPractice {
+    public static void main(String[] args) {
+        HashMap<String, String> dictionary = new HashMap<>();
+        dictionary.put("accuracy", "the condition or quality of being true, correct, or exact; freedom from error or defect; precision or exactness; correctness.");
+        dictionary.put("precision", "accuracy; exactness: ");
+        dictionary.put("faultless", "without fault, flaw, or defect; perfect.");
+        System.out.println(dictionary);
+    }
+}
