f008d28b3d022a858c1f238f6e57c8b4f51b8c4f
sod2003 <sod2003@gmail.com>
2024-01-19 23:55:31 -0500

    Attemptted to make a stream for the AppointmentSchedulerTests. Not finished.

--- Week09/Algorithms/groupwork/Appointment.java
+++ Week09/Algorithms/groupwork/Appointment.java
@@ -4,8 +4,10 @@ import java.time.Duration;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Appointment {
 	
@@ -44,27 +46,28 @@ public class Appointment {
 	// Give me a list of possible meeting times
 	static class SchedulingAssistant {
 		public static List<Appointment> getListOfOpenAppointments(List<Appointment> a1, List<Appointment> a2) {
-            List<Appointment> appointments = filledAppointmentList();
-            for(Appointment appointment : appointments) {
-                if(a1.contains(appointment)) {
-
-                }
-            }
-
+            List<Appointment> appointments = new ArrayList<Appointment>();
+            List<LocalTime> eligibleTimes = filledAppointmentList();
+            List<LocalTime> zt1 = a1.stream().map(appointment -> appointment.getStart().toLocalTime()).collect(Collectors.toList());
+            List<LocalTime> zt2 = a2.stream().map(appointment -> appointment.getStart().toLocalTime()).collect(Collectors.toList());
+            appointments = eligibleTimes.stream().filter(time -> {
+               return ((zt1.contains(time)) && (zt2.contains(time)));
+            })
+            .map(time -> ZonedDateTime.of(a1.get(0).getStart().toLocalDate(), time, a1.get(0).getStart().getZone()))
+            .map(zonedTime -> new Appointment(zonedTime, Duration.ofMinutes(30)))
+            .collect(Collectors.toList());
             return appointments;
         }
 
-        public static List<Appointment> filledAppointmentList() {
-            List<Appointment> appointments = new LinkedList<Appointment>();
+        public static List<LocalTime> filledAppointmentList() {
+            List<LocalTime> times = new LinkedList<LocalTime>();
             LocalTime timeRef = LocalTime.of(8, 0, 0);
             LocalTime endTime = LocalTime.of(17, 0, 0);
             for (;timeRef.isBefore(endTime); timeRef.plusMinutes(30)) {
-                appointments.add(new Appointment(ZonedDateTime.of(null, timeRef, null), Duration.ofMinutes(30)));
+                times.add(timeRef);
             }
-            return appointments;
+            return times;
         }
-
-        // public static double
 	}
 
 }
--- Week09/Algorithms/groupwork/AppointmentSchedulerTests.java
+++ Week09/Algorithms/groupwork/AppointmentSchedulerTests.java
@@ -1,7 +1,24 @@
 package Week09.Algorithms.groupwork;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppointmentSchedulerTests {
     public static void main(String[] args) {
-        
+        List<Appointment> matt = new ArrayList<Appointment>();
+        matt.add(new Appointment(ZonedDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(8, 30), ZoneId.systemDefault()), Duration.ofMinutes(30)));
+        matt.add(new Appointment(ZonedDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(10, 0), ZoneId.systemDefault()), Duration.ofMinutes(30)));
+        matt.add(new Appointment(ZonedDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(13, 30), ZoneId.systemDefault()), Duration.ofMinutes(30)));
+        List<Appointment> rod = new ArrayList<Appointment>();
+        rod.add(new Appointment(ZonedDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(8, 0), ZoneId.systemDefault()), Duration.ofMinutes(30)));
+        rod.add(new Appointment(ZonedDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(10, 30), ZoneId.systemDefault()), Duration.ofMinutes(30)));
+        rod.add(new Appointment(ZonedDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(13, 30), ZoneId.systemDefault()), Duration.ofMinutes(30)));
+
+        System.out.println(Appointment.SchedulingAssistant.getListOfOpenAppointments(matt, rod));
     }
 }
