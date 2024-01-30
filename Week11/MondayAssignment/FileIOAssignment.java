1e98922ff1d6996567d29e5784e58c1417422c43
sod2003 <sod2003@gmail.com>
2024-01-30 09:08:48 -0500

    Adding a method to find palindromes to the FileIOAssignment file.

--- Week11/MondayAssignment/FileIOAssignment.java
+++ Week11/MondayAssignment/FileIOAssignment.java
@@ -2,7 +2,6 @@ package Week11.MondayAssignment;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,7 +9,7 @@ import java.io.IOException;
 public class FileIOAssignment {
     private static final String path = "./Week11/MondayAssignment/";
 
-    public static void method1(String file) throws FileNotFoundException, IOException {
+    public static void method1(String file) throws IOException {
         String str, longestWord = "";
         int len = 0;
         try (BufferedReader in = new BufferedReader(new FileReader(path + file))) {
@@ -38,12 +37,36 @@ public class FileIOAssignment {
         }
     }
 
+    public static void findPalindromes(String file) throws IOException {
+        int count = 0;
+        try (BufferedReader in = new BufferedReader(new FileReader(path + file))) {
+            String str;
+            while ((str = in.readLine()) != null) {
+                if (palindrome(str)) {
+                    System.out.println(str);
+                    count++;
+                }
+            }
+            System.out.println("Palindromes in file: " + count);
+        }
+    }
+
+    public static boolean palindrome(String word) {
+        for(int i = 0, j = word.length() - 1; i < j; i++, j--) {
+            if (word.charAt(i) != word.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         try {
             method1("words_alpha.txt");
             method2("words_alpha.txt");
+            findPalindromes("words_alpha.txt");
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e);
         }
     }
 }
