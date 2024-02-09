aed6211aa908ac238f3775a4ba9bfa825ab5cb79
sod2003 <sod2003@gmail.com>
2024-02-09 13:23:33 -0500

    Adding PreparedStatement example to JdbcOverview.

--- Week12/JDBC/JdbcOverview.java
+++ Week12/JDBC/JdbcOverview.java
@@ -2,12 +2,18 @@ package Week12.JDBC;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JdbcOverview {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Scanner in = new Scanner(System.in);
+		System.out.print("Login Username: ");
+		String name = in.nextLine();
+		in.close();
         // step 1: Load Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -25,14 +31,41 @@ public class JdbcOverview {
             System.out.println("Done.");
 
             // Step 5: Process the results
-            while (rs.next()) {
-                String customer = rs.getString("customer_name");
-                int id = rs.getInt("id");
-                double balance = rs.getDouble("balance");
-                System.out.printf("%d. %10s $%.2f%n", id, customer, balance);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        //     while (rs.next()) {
+        //         String customer = rs.getString("customer_name");
+        //         int id = rs.getInt("id");
+        //         double balance = rs.getDouble("balance");
+        //         System.out.printf("%d. %10s $%.2f%n", id, customer, balance);
+        //     }
+        // } catch (SQLException e) {
+        //     System.out.println(e.getMessage());
+        // }
+        /////////////////////////////////////////////////////////////////////////////////////
+//			rs = stmt.executeQuery("SELECT * FROM account WHERE customer_name = \"" + name + "\";");
+//			System.out.println("Here is your account info");
+//			if (rs.next()) {
+//				String customer = rs.getString("customer_name");
+//				int id = rs.getInt("id");
+//				double balance = rs.getDouble("balance");
+//				System.out.printf("%d %20s $%.2f%n", id, customer, balance);
+//			} else {
+//				System.out.println("No account found");
+//			}
+			///////////////////////////////////////////////////////////////////////////////////////
+			PreparedStatement prstmt= conn.prepareStatement("SELECT * FROM account WHERE customer_name = ?;");
+			prstmt.setString(1, name); // not zero indexed
+			System.out.println("Here is your account info");
+			ResultSet result = prstmt.executeQuery();
+			if (result.next()) {
+				String customer = result.getString("customer_name");
+				int id = result.getInt("id");
+				double balance = result.getDouble("balance");
+				System.out.printf("%d %20s $%.2f%n", id, customer, balance);
+			} else {
+				System.out.println("No account found");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
     }
 }
