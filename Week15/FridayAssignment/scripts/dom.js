0fb01faba6009671f494a2b1e7b5ff418ea3e0c1
sod2003 <sod2003@gmail.com>
2024-03-01 20:02:21 -0500

    Making the bear images interactive on click.

--- Week15/FridayAssignment/scripts/dom.js
+++ Week15/FridayAssignment/scripts/dom.js
@@ -1,10 +1,28 @@
-let bears = document.getElementsByClassName("bears")
+let mainBear = document.getElementById("main-bear");
 
-for (let i = 0; i < bears.length; i++) {
-    console.log(bears[i].getAttribute("alt") +" is loaded");
-    bears[i].addEventListener("onClick", (this) => {
-        let mainBear = document.getElementById("main-bear");
-        mainBear.setAttribute("src", this.target.getAttribute("src"));
-        mainBear.setAttribute("alt", this.target.getAttribute("alt"))
-    });
+let brB = document.getElementById("brown-bear");
+brB.addEventListener("click", brownBear);
+
+let blB = document.getElementById("black-bear");
+blB.addEventListener("click", blackBear);
+
+let poB = document.getElementById("polar-bear");
+poB.addEventListener("click", polarBear);
+
+function brownBear() {
+    // console.log(bears[0].getAttribute("alt"));
+    mainBear.setAttribute("src", brB.getAttribute("src"));
+    mainBear.setAttribute("alt", brB.getAttribute("alt"));
+}
+
+function blackBear() {
+    // console.log(bears[1].getAttribute("alt"));
+    mainBear.setAttribute("src", blB.getAttribute("src"));
+    mainBear.setAttribute("alt", blB.getAttribute("alt"));
+}
+
+function polarBear() {
+    // console.log(bears[2].getAttribute("alt"));
+    mainBear.setAttribute("src", poB.getAttribute("src"));
+    mainBear.setAttribute("alt", poB.getAttribute("alt"));
 }
